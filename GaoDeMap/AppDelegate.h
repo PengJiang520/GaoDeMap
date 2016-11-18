8a33281e5fe406b74a4a202ac0061142f9efe559
JiangPeng <907487282@qq.com>
2016-11-18 09:18:14 +0800

    add all files

---
+++ GaoDeMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GaoDeMap
+//
+//  Created by PengJiang on 2016/11/18.
+//  Copyright © 2016年 杭州凯立通信有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GaoDeMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GaoDeMap
+//
+//  Created by PengJiang on 2016/11/18.
+//  Copyright © 2016年 杭州凯立通信有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
